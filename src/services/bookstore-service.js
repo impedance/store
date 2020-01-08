43760ac56c088b3fa806d8e7ccbd1ca485cde3a4
impedance <2275399@gmail.com>
2020-01-08 19:03:20 +0300

    making css styles

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,11 +5,15 @@ export default class BookstoreService {
     return [
       { id: 1, 
         title: 'Production-Ready Microservices',
-        author: 'Susan J. Fowler' 
+        author: 'Susan J. Fowler',
+        price: 32,
+        coverImage: "https://www.ida.liu.se/~TDDB84/pictures/HeadFirstDesignPatterns.jpg"
       },
       { id: 2, 
         title: 'Release it!',
-        author: 'Michael T. Nygard' 
+        author: 'Michael T. Nygard',
+        price: 45,
+        coverImage: "https://www.safaribooksonline.com/library/cover/9781491969540/360h/"
       },
     ];
   }
